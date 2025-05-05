15734353d8556e0b29bd557acc1218e91c0602c3
James Orson <jamesaorson@gmail.com>
2025-05-05 15:26:15 -0700

    29: Install custom component example via config_flow

--- src/autobutler/home-assistant/components/autobutler/__init__.py
+++
@@ -1,8 +0,0 @@
-DOMAIN = "autobutler"
-
-
-def setup(hass, config):
-    hass.states.set(f"{DOMAIN}.hello.world", "from the autobutler")
-
-    # Return boolean to indicate that initialization was successful.
-    return True
