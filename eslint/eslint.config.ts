7dfd4c455f79f64d9d22a08eb2c5d64f7a176602
James Orson <jamesaorson@gmail.com>
2025-05-30 17:44:37 -0700

    fix-lint: Remove comment

--- eslint/eslint.config.ts
+++ eslint/eslint.config.ts
@@ -2,7 +2,6 @@ import { withNuxt } from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt({
   rules: {
-    //   79:13  warning  Disallow self-closing on HTML void elements (<input/>)  vue/html-self-closing
     "vue/html-self-closing": [
       "off",
       {
