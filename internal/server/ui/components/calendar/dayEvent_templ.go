23cc6dd81d0aa391e8e08f6e128fbf0bae518e1f
James Orson <jamesaorson@gmail.com>
2025-09-11 17:21:16 -0700

    192: Remove whitespace weirdness
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- internal/server/ui/components/calendar/dayEvent_templ.go
+++ internal/server/ui/components/calendar/dayEvent_templ.go
@@ -51,7 +51,7 @@ func dayEvent(event calendar.Event) templ.Component {
 		var templ_7745c5c3_Var2 string
 		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(trimEventTitle(event, 20))
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/server/ui/components/calendar/dayEvent.templ`, Line: 20, Col: 102}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/server/ui/components/calendar/dayEvent.templ`, Line: 19, Col: 102}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
