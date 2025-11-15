fe7980eae5fb102cc3b42b3b3f350d5cadf357b9
James Orson <jamesaorson@gmail.com>
2025-11-15 11:27:59 -0500

    419: Remove bad comment
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- cmd/autobutler/version/version.go
+++ cmd/autobutler/version/version.go
@@ -14,8 +14,6 @@ func Cmd() *cobra.Command {
 		Short: "Display version information for Autobutler CLI",
 		Long:  `The version command provides the current version of the Autobutler CLI and its components.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			// Here you would typically retrieve the version information from a variable or a configuration file.
-			// For demonstration purposes, we'll use a hardcoded version string.
 			version := versionutil.GetVersion()
 			fmt.Println(version.VersionString())
 		},
